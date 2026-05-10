275fb04c969bb1762d6832a03aec0852cdd034d0
Jatin Mandwani <masterjatinmandwani@gmail.com>
2026-05-10 14:52:46 +0530

    initialize an empty dictionary and add list items as keys and indexes as key values

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from traceback import print_tb
+
 import numpy as np
 
 genre_list = np.array([
@@ -18,5 +20,7 @@ genre_list = np.array([
     "Thriller"
 ])
 
+genre_dict = {}
+
 for i in range(len(genre_list)):
-    print(f"{i}: {genre_list[i]}")
+    genre_dict.update({genre_list[i]: i})
