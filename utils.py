89e2fd2a92bbba7d86a3b1b6eaf5e10823dcba74
Jatin Mandwani <masterjatinmandwani@gmail.com>
2026-05-19 00:13:06 +0530

    Major Structural Changes: Add 'movies.json' for cross file input output, 'utils.py' for all the functions, and delete 'app.py' for now and replace 'movies.py' with 'movies.json'

--- utils.py
+++ utils.py
@@ -1,9 +1,84 @@
 import numpy as np
+import json
 from movies import genre_dict, movie_vectors, genre_list
 
+with open("movies.json", "r") as mfile:
+    data = json.load(mfile)
+# Genre List and Dict Updation
+
+def genre_updation():
+
+    while True:
+        new_genre = input("\nEnter a new genre (or type 'exit' to quit): ").strip()
+
+        if not new_genre:
+            print("Genre cannot be empty")
+            continue
+
+        elif new_genre == "exit":
+            break
+
+        elif new_genre.lower() in [genre.lower() for genre in data["genre_list"]]:
+            print(f"The genre '{new_genre}' already exists")
+            continue
+
+        else:
+            data["genre_list"].append(new_genre)
+
+
+    for i in range(len(data["genre_list"])):
+        data["genre_dict"].update({data["genre_list"][i]: i})
+
+    with open("movies.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+# Input handling for Movies and Genres
+
+def input_movies_and_genres():
+    while True:
+        movie_input = input("Enter movie name (or type 'exit' to quit): ").strip()
+
+        if movie_input.lower() == "exit":
+            print("Exiting Movie Input...")
+            break
+
+        elif not movie_input:
+            print("Input cannot be empty!")
+            continue
+
+        else:
+            zero_list = [0] * len(data["genre_list"])
+            data["movie_vectors"].update({movie_input: zero_list})
+
+        while True:
+            genre_input = (input(f"Enter the genre that needs for the movie '{movie_input}' (or type 'exit' to quit): ")
+                           .strip())
+
+            if genre_input.lower() == "exit":
+                print("Exiting Genre Input...")
+                break
+
+            if not genre_input:
+                print("Genre cannot be empty!")
+                continue
+
+            elif genre_input.lower() not in [genre.lower() for genre in data["genre_list"]]:
+                print("Genre not available! Kindly recheck")
+                continue
+
+            elif genre_input.lower() in [genre.lower() for genre in data["genre_list"]]:
+                for i in range(len(data["genre_list"])):
+                    data["genre_dict"].update({data["genre_list"][i]: i})
+
+                data["movie_vectors"][movie_input][data["genre_dict"][genre_input]] = 1
+
+    with open("movies.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+input_movies_and_genres()
 # Dot Product Function
 
-def dot(v1: np.ndarray, v2: np.ndarray) -> int:
+def dot(v1: list, v2: list) -> int:
 
     dot_product = 0
 
